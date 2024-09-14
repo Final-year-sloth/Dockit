265d23104be63262658c49d52c92ba4b38b8a7c4
Rounak Singh <rounak3907@gmail.com>
2024-09-14 21:42:38 +0530

    Add SideBar Component

--- next-frontend/src/app/page.tsx
+++ next-frontend/src/app/page.tsx
@@ -1,7 +1,18 @@
+'use client';
+/*Import the Image component from Next.js.*/
 import Image from "next/image";
 
+/*Import the Sidebar component from the components directory.*/
+import Sidebar from "@/components/sidebar";
+
+/*The Home page component.*/
 export default function Home() {
+  /*Render the Home page content.*/
   return (
-   <div className="container"><h1>Home</h1></div>
+    /*The container element for the Home page.*/
+    <div className="container">
+      {/*Render the Sidebar component.*/}
+      <Sidebar/>
+    </div>
   );
 }
