475a4d4145eefe3f47ffa4022e7661fc05a7bdf1
Arko298 <choudhurysayantan03@gmail.com>
2024-09-18 20:01:51 +0530

    Layouts are being added

---
+++ next-frontend/src/layouts/LandingLayout.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/components/Navbar"
+
+import { ReactNode } from "react";
+interface LandingLayoutProps{
+  children: ReactNode;
+}
+const LandingLayout = ({ children }: LandingLayoutProps) => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <Navbar />
+      <main className="p-4">{children}</main>
+    </div>
+    )
+}
+export default LandingLayout;
+
